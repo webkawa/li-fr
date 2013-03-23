bb548d684f579bd3e70ee78b28f37f058a965964
kawa <kawa@kawa-PC>
2013-03-23 17:12:18 +0100

    Add constants

--- mode/full/js/init/constants.js
+++ mode/full/js/init/constants.js
@@ -2,3 +2,11 @@
  * ------------
  * Executes and memorize a list of tests relative to the CSS constants. */
 
+var constants;
+
+/* Todo : tests */
+$(document).ready(function() {
+    constants = {
+        bodyMinHeight   :   parseInt($("div#body").css("min-height"))
+    };
+});
